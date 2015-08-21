c79870ea90576bf7eae5f7703658b13549fd441c
Kash Nouroozi <knouroozi@gmail.com>
2015-08-20 21:09:25 -0400

    Temp README and enough package info to publish

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 Geocoder
 ========
 
-** TODO: Add description **
+Add to your deps, add it to the list of applications.
+
+```elixir
+Geocoder.geocode("Toronto, ON")
+Geocoder.reverse_geocode({43.653226, -79.383184})
+
+# Or use Geocoder.call, which calls the above depending on
+# whether a string was passed in or a 2-tuple
+```
--- mix.exs
+++ mix.exs
@@ -3,11 +3,19 @@ defmodule Geocoder.Mixfile do
 
   def project do
     [app: :geocoder,
+     description: "A simple, efficient geocoder/reverse geocoder with a built-in cache."
      version: "0.0.1",
      elixir: "~> 1.0",
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
+  end
+
+  def package do
+    [licences: ["MIT"],
+     contributors: ["Kash Nouroozi"],
+     links: %{"Github" => "https://githu.com/knrz/geocoder"}]
   end
 
   def application do
