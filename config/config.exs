58e3ac2071693190471ff7d24942ff6a69c3a369
Michael Bianco <iloveitaly@gmail.com>
2020-11-09 06:22:08 -0700

    Better env default and mix formatting

--- config/config.exs
+++ config/config.exs
@@ -4,12 +4,17 @@ config :geocoder, :worker_pool_config,
   size: 4,
   max_overflow: 2
 
-case System.get_env("PROVIDER") || "openstreetmaps" do
+case System.get_env("PROVIDER", "openstreetmaps") do
   "google" ->
-    config :geocoder, :worker, provider: Geocoder.Providers.GoogleMaps, key: System.get_env("API_KEY", "NO_API_KEY")
+    config :geocoder, :worker,
+      provider: Geocoder.Providers.GoogleMaps,
+      key: System.get_env("API_KEY", "NO_API_KEY")
+
   "opencagedata" ->
-    config :geocoder, :worker, provider: Geocoder.Providers.OpenCageData, key: System.get_env("API_KEY", "NO_API_KEY")
+    config :geocoder, :worker,
+      provider: Geocoder.Providers.OpenCageData,
+      key: System.get_env("API_KEY", "NO_API_KEY")
+
   "openstreetmaps" ->
     config :geocoder, :worker, provider: Geocoder.Providers.OpenStreetMaps
 end
-
