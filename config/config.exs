039c5cdde37a2192d20d5b05a69049205b1d67d8
Kyle Boe <kyle@boe.codes>
2022-05-04 18:03:48 -0700

    Update config to include data to statisfy testing Fake provider

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,69 @@ config :geocoder, :worker_pool_config,
   size: 4,
   max_overflow: 2
 
+config :geocoder, Geocoder.Worker,
+  data: %{
+    "1991 15th Street, Troy, NY 12180" => %{
+      lat: 0.0,
+      lon: 0.0,
+      bounds: %{
+        top: 0.0,
+        right: 0.0,
+        bottom: 0.0,
+        left: 0.0,
+      },
+      location: %{
+        street_number: "1991",
+        street: "15th Street",
+        city: "Troy",
+        county: "Rensselaer County",
+        country_code: "us",
+        postal_code: "12180",
+      }
+    },
+    "Dikkelindestraat 46, 9032 Wondelgem, Belgium" => %{
+      lat: 51.0775527,
+      lon: 3.7074204,
+      bounds: %{
+        bottom: 51.077496,
+        left: 3.7073144,
+        right: 3.7075457,
+        top: 51.0776028,
+      },
+      location: %{
+        city: "Ghent",
+        country: "Belgium",
+        country_code: "be",
+        county: "Gent",
+        formatted_address: "Dikkelindestraat 46, 9032 Ghent, Belgium",
+        postal_code: "9032",
+        state: "East Flanders",
+        street: "Dikkelindestraat",
+        street_number: "46"
+      }
+    },
+    {51.0775264, 3.7073382} => %{
+      lat: 51.0775527,
+      lon: 3.7074204,
+      bounds: %{
+        bottom: 51.077496,
+        left: 3.7073144,
+        right: 3.7075457,
+        top: 51.0776028,
+      },
+      location: %{
+        city: "Ghent",
+        country: "Belgium",
+        country_code: "be",
+        county: "Gent",
+        formatted_address: "Dikkelindestraat 46, 9032 Ghent, Belgium",
+        postal_code: "9032",
+        state: "East Flanders",
+        street: "Dikkelindestraat",
+        street_number: "46"
+      }
+    }
+  }
 case System.get_env("PROVIDER", "openstreetmaps") do
   "google" ->
     config :geocoder, :worker,
